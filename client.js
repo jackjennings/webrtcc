a74c8a0da45cdc614d28d9b6832afd308188ad13
Jack Jennings <jack@standard-library.com>
2018-07-13 18:19:42 -0700

    Replicate hypercore?

--- client.js
+++ client.js
@@ -1,11 +1,20 @@
+const pump = require("pump");
 const signalhub = require("signalhub");
 const SimplePeer = require("simple-peer");
 const webrtcSwarm = require("webrtc-swarm");
+
+const hypercore = require("hypercore");
+const ram = require("random-access-memory");
+
+const feed = hypercore(() => ram(), { valueEncoding: "utf-8" });
 
 const hub = signalhub("asdf", ["http://localhost:8080"]);
 const swarm = webrtcSwarm(hub);
 
 swarm.on("peer", (peer, id) => {
-  console.log(id);
-  peer.on("data", data => console.log(data))
+  if (id !== swarm.me) {
+    pump(peer, feed.replicate({ live: true }), peer);
+  }
 });
+
+feed.on("append", console.log.bind(console));
--- server.js
+++ server.js
@@ -1,16 +1,30 @@
+const hypercore = require("hypercore");
+const pump = require("pump");
 const signalhub = require("signalhub");
 const SimplePeer = require("simple-peer");
-const wrtc = require("wrtc");
 const webrtcSwarm = require("webrtc-swarm");
-const K = require("kefir");
+const wrtc = require("wrtc");
+
+const feed = hypercore("./storage", { valueEncoding: "utf-8" });
 
 const hub = signalhub("asdf", ["http://localhost:8080"]);
 const swarm = webrtcSwarm(hub, { wrtc });
 
-const connect = K.fromEvents(swarm, "peer", (peer, id) => [peer, id]);
+const capturePeer = (peer, id) => ({ peer, id });
+const connect = K.fromEvents(swarm, "peer", capturePeer);
 
-connect.observe(([peer, id]) => {
-  console.log(swarm.peers.length);
-  console.log(id);
-  peer.send(`hey ${id}`);
+setInterval(() => {
+  feed.append('hello');
+}, 1000);
+
+swarm.on("connect", (peer, id) => {
+  console.log("connected to:", id);
+  pump(peer, feed.replicate({ live: true }), peer);
+});
+
+hub.subscribe("all").on("data", data => {
+  if (swarm.me !== data.from) {
+    const data = swarm.wrap({ type: "connect", from: swarm.me }, "all");
+    hub.broadcast("all", data);
+  }
 });
