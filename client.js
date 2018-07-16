083c3a3d15ead89733d5f22fb691fae1bf41c8a8
Jack Jennings <jack@standard-library.com>
2018-07-16 11:30:46 -0700

    Cleanup and add logs

--- client.js
+++ client.js
@@ -12,9 +12,26 @@ const hub = signalhub("asdf", ["http://localhost:8080"]);
 const swarm = webrtcSwarm(hub);
 
 swarm.on("peer", (peer, id) => {
+  console.log("connected to: ", id);
   if (id !== swarm.me) {
     pump(peer, feed.replicate({ live: true }), peer);
   }
 });
 
-feed.on("append", console.log.bind(console));
+// None of these ever log…
+
+feed.on("append", () => console.log("appended"));
+feed.on("append", (index, data) => console.log("downloaded", data));
+feed.on("error", e => console.log(e));
+
+const stream = feed.createReadStream({
+  live: true,
+  tail: true
+});
+
+stream.on("data", d => console.log(d));
+
+setInterval(() => {
+  console.log("Length: ", feed.length)
+  feed.head(d => console.log("Head: ", d));
+}, 1000);
--- server.js
+++ server.js
@@ -10,21 +10,12 @@ const feed = hypercore("./storage", { valueEncoding: "utf-8" });
 const hub = signalhub("asdf", ["http://localhost:8080"]);
 const swarm = webrtcSwarm(hub, { wrtc });
 
-const capturePeer = (peer, id) => ({ peer, id });
-const connect = K.fromEvents(swarm, "peer", capturePeer);
-
-setInterval(() => {
-  feed.append('hello');
-}, 1000);
-
 swarm.on("connect", (peer, id) => {
-  console.log("connected to:", id);
+  console.log("connected to: ", id);
   pump(peer, feed.replicate({ live: true }), peer);
 });
 
-hub.subscribe("all").on("data", data => {
-  if (swarm.me !== data.from) {
-    const data = swarm.wrap({ type: "connect", from: swarm.me }, "all");
-    hub.broadcast("all", data);
-  }
-});
+setInterval(() => {
+  console.log("append");
+  feed.append("hello");
+}, 1000);
